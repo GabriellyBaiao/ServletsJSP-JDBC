90ac75be5843c83ae92d318f9c4dee8ff95de08b
Gabrielly Baião <gabriellybaiao95@gmail.com>
2024-04-26 10:16:57 -0300

    [PTI] feat: Keeping the data on the screen and redirecting with EL JSP

--- curso-jsp/src/main/java/model/ModelLogin.java
+++ curso-jsp/src/main/java/model/ModelLogin.java
@@ -6,12 +6,32 @@ public class ModelLogin implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	private String login;
 	
+	private Long id;
+	private String nome;
+	private String email;
+	private String login;
 	private String senha;
 	
-	
-	 public String getSenha() {
+	 public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
 		return senha;
 	}
 	public void setSenha(String senha) {
