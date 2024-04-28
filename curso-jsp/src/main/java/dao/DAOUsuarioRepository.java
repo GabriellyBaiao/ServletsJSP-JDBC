751f409b4e11470fe5c4529081ade4daa26f9759
Gabrielly Baião <gabriellybaiao95@gmail.com>
2024-04-28 18:53:46 -0300

    [PTI] feat: Creating the SQL and method that queries the user by login

--- curso-jsp/src/main/java/dao/DAOUsuarioRepository.java
+++ curso-jsp/src/main/java/dao/DAOUsuarioRepository.java
@@ -2,6 +2,8 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import connection.SingleConnectionBanco;
 import model.ModelLogin;
@@ -16,7 +18,7 @@ public class DAOUsuarioRepository {
 		connection = SingleConnectionBanco.getConnection();
 	}
 	
-	public void gravarUsuario(ModelLogin objeto) throws Exception {
+	public ModelLogin gravarUsuario(ModelLogin objeto) throws Exception {
 		
 		String sql = "INSERT INTO model_login(login, senha, nome, email)  VALUES (?, ?, ?, ?);";
 		PreparedStatement preparedSql = connection.prepareStatement(sql);
@@ -29,6 +31,27 @@ public class DAOUsuarioRepository {
 		preparedSql.execute();
 		
 		connection.commit();
+		
+		return this.consultaUsuario(objeto.getLogin());		
+	}
+	
+	public ModelLogin consultaUsuario(String login) throws Exception {
+		
+		ModelLogin modelLogin = new ModelLogin();
+		
+		String sql = "select * from model_login where login= upper('"+login+"')";
+		
+		PreparedStatement statement = connection.prepareStatement(sql);
+		ResultSet resultado = statement.executeQuery();
+		
+		while(resultado.next()) /*Se tem resultado*/ {
+			modelLogin.setId(resultado.getLong("id"));
+			modelLogin.setEmail(resultado.getString("email"));
+			modelLogin.setLogin(resultado.getString("login"));
+			modelLogin.setSenha(resultado.getString("senha"));
+			modelLogin.setNome(resultado.getString("nome"));
+		}
+		return modelLogin;
 		
 	}
 
